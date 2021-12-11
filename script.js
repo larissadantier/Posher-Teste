723b8c540bc754fe760c03b89e1c600d735402a1
larissadantier <larissa_dantier@hotmail.com>
2021-12-11 20:40:03 -0300

    Subsequent, Duplicates and Palindromes

---
+++ script.js
@@ -0,0 +1,50 @@
+// Dado dois arrays de inteiros não vazios, determine se os valores do segundo array estão na mesma ordem no primeiro array.
+function isSubsequent() {
+  const inputFirst = document
+    .getElementById("firstArrayA")
+    .value.split(" ")
+    .map(Number);
+
+  const inputSecond = document
+    .getElementById("secondArrayA")
+    .value.split(" ")
+    .map(Number);
+
+  const sequence = (arrayA, arrayB) => {
+    let filterArray = arrayA.filter((text) => arrayB.includes(text));
+
+    return filterArray.toString() === arrayB.toString();
+  };
+
+  return alert(`Resultado: [${sequence(inputFirst, inputSecond)}]`);
+}
+
+// Dado um array de strings não vazio, remove os dados duplicados em sequência.
+function removeDuplicates() {
+  const inputDuplicates = document
+    .getElementById("duplicateArray")
+    .value.split(" ")
+    .map(String);
+  const removeDuplicate = [...new Set(inputDuplicates)];
+  return alert(`Resultado: [${removeDuplicate}]`);
+}
+
+//Dado um array de strings não vazio, retorna um array de booleans indicando se a string é ou não um palíndromo.
+function verifyPalindromes() {
+  const inputPalindromo = document
+    .getElementById("palindromoArray")
+    .value.split(" ")
+    .map(String);
+
+  const resultNewArray = [];
+
+  inputPalindromo.forEach((item) => {
+    resultNewArray.push(logicCheckPalíndromo(item));
+  });
+
+  return alert(`Resultado: [${resultNewArray}]`);
+}
+
+function logicCheckPalíndromo(word) {
+  return word.split("").reverse().join("") === word;
+}
